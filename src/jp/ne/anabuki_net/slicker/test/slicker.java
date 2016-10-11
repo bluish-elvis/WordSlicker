aca4f7667b6006cd47ecdbb9aff74560a093dd7c
bel_mw_ <14010710@anabuki-net.ne.jp>
2016-10-11 11:25:28 +0900

    tester:get input and draw

--- src/jp/ne/anabuki_net/slicker/test/slicker.java
+++ src/jp/ne/anabuki_net/slicker/test/slicker.java
@@ -4,10 +4,15 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.KeyListener;
 import org.newdawn.slick.SlickException;
 
 /** @author bluish */
-public class slicker extends BasicGame{
+public class slicker extends BasicGame implements KeyListener{
+	float x=304, y=184;
+	static final float SPEED=0.1f;
+	String str="";
 	public slicker(){
 		super("Wizard game");
 	}
@@ -24,7 +29,24 @@ public class slicker extends BasicGame{
 
 	@Override public void init(GameContainer container) throws SlickException{}
 
-	@Override public void update(GameContainer container, int delta) throws SlickException{}
+	@Override public void update(GameContainer gc, int delta) throws SlickException{
+		float move=SPEED*delta;
+		Input input=gc.getInput();
+		if(input.isKeyDown(input.KEY_LEFT)) x-=move;
+		else if(input.isKeyDown(input.KEY_RIGHT)) x+=move;
 
-	public void render(GameContainer container, Graphics g) throws SlickException{}
+		if(input.isKeyDown(input.KEY_UP)) y-=move;
+		else if(input.isKeyDown(input.KEY_DOWN)) y+=move;
+
+	}
+
+	public void render(GameContainer container, Graphics g) throws SlickException{
+		g.drawRect(x-16, y-16, 32, 32);
+		g.drawString(str, x-4, y-9);
+	}
+
+    @Override
+    public void keyPressed(int key, char c) {
+    	str=Input.getKeyName(key);
+    }
 }
