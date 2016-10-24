dc13d42ef794dadf8f6469ccdc4bddbbf526fcc2
K-Y-0107 <14010711@anabuki-net.ne.jp>
2016-10-24 11:46:52 +0900

    仮のSQLです Itemはまだ実装されていません

---
+++ wordslicker.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2016 年 10 朁E24 日 04:38
+-- サーバのバージョン： 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wordslicker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `course`
+--
+
+CREATE TABLE IF NOT EXISTS `course` (
+`CID` int(6) NOT NULL,
+  `s_name` char(36) NOT NULL,
+  `d_name` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `records`
+--
+
+CREATE TABLE IF NOT EXISTS `records` (
+  `UID` int(8) NOT NULL,
+  `CID` int(6) NOT NULL,
+  `Score` int(64) NOT NULL,
+  `time` int(8) NOT NULL,
+  `flowless` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+`UID` int(8) NOT NULL,
+  `name` char(36) NOT NULL,
+  `pass` char(64) NOT NULL,
+  `email` char(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+ ADD PRIMARY KEY (`CID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`UID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+MODIFY `CID` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `UID` int(8) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
