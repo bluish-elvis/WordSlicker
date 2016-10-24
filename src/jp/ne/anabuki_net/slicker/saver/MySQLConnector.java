0c5a4f1bca328e5e914e30fe8407b9033472340d
K-Y-0107 <14010711@anabuki-net.ne.jp>
2016-10-24 10:09:31 +0900

    MySQLConnector.java

---
+++ src/jp/ne/anabuki_net/slicker/saver/MySQLConnector.java
@@ -0,0 +1,40 @@
+package jp.ne.anabuki_net.slicker.saver;
+
+import java.sql.*;
+
+public class MySQLConnector {
+    public static void main(String[] args) {
+        Connection con = null;
+        try {
+            // JDBCドライバのロード - JDBC4.0（JDK1.6）以降は不要
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            // MySQLに接続
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+            System.out.println("MySQLに接続できました。");
+
+            Statement stm = con.createStatement();
+            String sql = "";
+            ResultSet rs = stm.executeQuery(sql);
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println("取得結果 -> " + id + ":" + name);
+
+
+       } 
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            System.out.println("JDBCドライバのロードに失敗しました。");
+        } catch (SQLException e) {
+            System.out.println("MySQLに接続できませんでした。");
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("MySQLのクローズに失敗しました。");
+                }
+            }
+        }
+    }
+}
